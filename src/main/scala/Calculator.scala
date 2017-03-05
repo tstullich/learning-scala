70626fea25e5edf65b94f1483073697293cc041e
Tim Stullich <timstullich@gmail.com>
2017-03-04 21:41:44 -0800

    Added some practice routines

--- src/main/scala/Calculator.scala
+++ src/main/scala/Calculator.scala
@@ -1,6 +1,37 @@
-/**
-  * Created by tim on 2/25/17.
-  */
 object Calculator {
 
+  def add(a: Int, b: Int): Int = {
+    a + b
+  }
+
+  def add(a: Int): Int => Int = (b: Int) => a + b
+
+  def multiply(a: Int, b: Int): Int = {
+    a * b
+  }
+
+  def subtract(a: Int, b: Int): Int = {
+    a - b
+  }
+
+  def abs(x: Int): Int = {
+    if (x < 0) -x
+    else x
+  }
+
+  def factorial(x: Int): Int = {
+    def loop(x: Int, acc: Int): Int = {
+      if (x == 0) acc
+      else loop(x - 1, x * acc)
+    }
+    loop(x, 1)
+  }
+
+//  def curry[A, B, C](f: (A, B) => C): A => (B => C) = (a: A, b: B) => f(a, b)
+
+  def main(args: Array[String]): Unit = {
+    println(factorial(3))
+    println(factorial(4))
+    println(add(4)(5))
+  }
 }
